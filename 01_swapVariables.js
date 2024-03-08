42f8236703348e81c7ea79868ff2a1cb8f5e99dd
patiltejaswini100 <patiltejaswini100@gmail.com>
2024-03-08 17:46:37 +0530

    form prepared and swap digits
    form prepared and swap digits

---
+++ 01_swapVariables.js
@@ -0,0 +1,47 @@
+
+console.log("WAP to swap variable values using third variable");
+var n1 = 100; // 200
+var n2 = 200; // 100
+console.log("======== Before Swap ==========");
+console.log("n1:", n1, " n2", n2);
+
+console.log("======== After Swap ==========");
+var temp = n1;
+n1 = n2;
+n2 = temp;
+console.log("n1:", n1, " n2:", n2);
+
+
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("========1-Before-swap========");
+console.log("sweety:", sweety);
+console.log("cutie:",cutie);
+
+console.log("========1-after-swap========");
+var temp="Sweety"
+sweety = cutie;
+cutie = temp;
+console.log("sweety:",sweety);
+console.log("cutie:",cutie);
+
+var num1=1000;
+var num2=2000;
+var num3=3000;
+console.log("========2-Before-swap========");
+console.log("num 1:",num1);
+console.log("num 2:",num2);
+console.log("num 3:",num3);
+
+console.log("========2-after-swap========");
+
+var temp=num1
+
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("num1:",num1);
+
+console.log("num2:",num2);
+
+console.log("num3:",num3);
---
+++ 01_variableAssign.js
@@ -0,0 +1,38 @@
+console.log("==========Step-1==========");
+
+var fullName; 
+console.log("Full Name variable value before Initialization:", fullName);
+
+var fullName = "Tejaswini Patil"
+console.log("Full Name variable value after Initialization:", fullName);
+
+var fullName = "Tejaswini Mohit Patil"
+console.log("Full Name variable value updated:", fullName);
+
+console.log("==========Step-2==========");
+
+var myLove = "I love only JS";
+console.log("My Love:", myLove);
+
+console.log("==========Step-3==========");
+
+var firstName = "Tejaswini"
+console.log("First Name:", firstName);
+
+var lastName="Patil";
+console.log("Last Name:", lastName);
+
+var age="25";
+console.log("Age:", age);
+
+var address="Laxmi Chowk, Hinjewadi, Pune";
+console.log("Address:", address);
+
+var pinCode="411057";
+console.log("Pin Code:", pinCode);
+
+var gender="Female";
+console.log("Gender:", gender);
+
+var isMarried="Yes";
+console.log("Is Married:", isMarried);
