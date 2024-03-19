8635287ace51b48dc433d51e3d1207082bfa7294
patiltejaswini100 <patiltejaswini100@gmail.com>
2024-03-19 18:11:41 +0530

    for-loop, while-loop, do-while-loop, string-transversing
    for-loop, while-loop, do-while-loop, string-transversing

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,41 @@
+console.log("=========Numbers from 5 to 15 incrementing by 1=========");
+for (let index = 5; index <= 15; index++) {
+    console.log(index);
+    
+}
+
+console.log("=========Numbers from 50 to 40 decrementing by 1=========");
+for (let i = 50; i >= 40; i--) {
+    console.log(i);
+    
+}
+
+console.log("=========First 15 odd numbers=========");
+for (let odd = 1; odd < 30; odd=odd+2) {
+    console.log(odd);
+    
+}
+
+console.log("=========First 10 even numbers=========");
+for (let even = 0; even < 20; even=even+2) {
+    console.log(even);
+    
+}
+
+console.log("=========Table of five=========");
+for (let f = 5; f <= 50; f=f+5) {
+    console.log(f);
+    
+}
+
+console.log("=========Table of Ten=========");
+for (let t = 10; t <= 100; t=t+10) {
+    console.log(t);
+    
+}
+
+console.log("=========Table of Ten in reverse order=========");
+for (let tr = 100; tr >= 10; tr=tr-10) {
+    console.log(tr);
+    
+}
